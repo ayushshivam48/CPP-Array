5193d2785d1924557eb878b419a9958411f6df51
Ayush Shivam <166586488+ayushshivam48@users.noreply.github.com>
2024-08-15 10:24:15 +0530

    Add files via upload

---
+++ sumofarrayelements.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+int arr[5];
+cout<<"Enter array elements: ";
+for(int i=0;i<=4;i++){
+    cin>>arr[i];
+}
+int sum=0;
+for(int i=0;i<=4;i++){
+    sum+=arr[i];
+   }
+   cout<<"sum of all no in the array:"<<sum;
+}
