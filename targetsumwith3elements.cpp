b6a60f289b0fb2061420198be618d8aaad32daca
Ayush Shivam <166586488+ayushshivam48@users.noreply.github.com>
2024-08-21 12:21:19 +0530

    Add files via upload

---
+++ targetsumwith3elements.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[]={3,1,2,4,0,6};
+    int targetsum=5;
+    int size=6;
+    int triplets=0;
+    for(int i=0;i<size;i++){
+        for(int j=i+1;j<size;j++){
+             for(int k=j+1;k<size;k++){
+                if (arr[i]+arr[j]+arr[k]==targetsum){
+                triplets++;
+                }
+            }
+        }
+    }
+    cout<<triplets<<endl;
+}
