c65f1a10f4cbd4ec908d12e5ef9bcd1a766a39ea
Ayush Shivam <166586488+ayushshivam48@users.noreply.github.com>
2024-08-24 11:34:52 +0530

    Add files via upload

---
+++ linearsearch.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[]={3,9,18,11,7};
+    int key=11;
+    int ans=-1;
+    for(int i=0;i<5;i++){
+        if(arr[i]==key){
+            ans=i;
+            break;
+        }
+    }
+    cout<<ans<<endl;
+}
